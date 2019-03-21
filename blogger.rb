a8beb65aaf64663498eff0d983f7ac06c1be5e1e
Rustam Islamov <risc.box@gmail.com>
2019-03-21 22:30:44 +0500

    Clear.

--- blogger.rb
+++
@@ -1,6 +0,0 @@
-require "jekyll-import";
-    JekyllImport::Importers::Blogger.run({
-      "source"                => "blog-03-21-2019.xml",
-      "no-blogger-info"       => false, # not to leave blogger-URL info (id and old URL) in the front matter
-      "replace-internal-link" => true, # replace internal links using the post_url liquid tag.
-    })
